76e8c6cdbfdde8d76cf8ff116d06071e7d0dcbaa
D-Andrea <diogodandrea@outlook.com>
2023-11-21 19:14:25 +0100

    User authenticator and last changes

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from contact.forms import RegisterForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -36,7 +37,6 @@ def login_view(request):
             auth.login(request, user)
             messages.success(request, 'Logged in successfully.')
             return redirect('contact:index')
-        messages.error(request, 'Error')
 
     return render(
         request,
@@ -46,7 +46,35 @@ def login_view(request):
         }
     )
 
-
+@login_required(login_url='contact:login')
 def logout_view(request):
     auth.logout(request)
     return redirect('contact:login')
+
+@login_required(login_url='contact:login')
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method != 'POST':
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
+
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form.save()
+    return redirect('contact:login')
+
